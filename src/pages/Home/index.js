2b74cd4e29366e89e2a289a7a57654733a1386b0
Eduardo Gomes Heleno <eduardo.gheleno@gmail.com>
2021-08-12 12:28:54 -0300

    :sparkles: Card slider done

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-// import Carousel from 'react-multi-carousel';
+import Carousel from 'react-multi-carousel';
 
-import { HeartOutlined, ShoppingCartOutlined } from '@ant-design/icons';
+import { HeartOutlined, HeartFilled, ShoppingCartOutlined } from '@ant-design/icons';
 
 import {
   HomeSection,
@@ -21,25 +21,32 @@ import Banner from '../../components/Banner';
 import itemImage from '../../static/images/itemImage.png';
 
 function Home() {
-  // const responsive = {
-  //   desktop: {
-  //     breakpoint: { max: 3000, min: 1024 },
-  //     items: 3,
-  //     slidesToSlide: 3
-  //   },
-  //   tablet: {
-  //     breakpoint: { max: 1024, min: 464 },
-  //     items: 2,
-  //     slidesToSlide: 2
-  //   },
-  //   mobile: {
-  //     breakpoint: { max: 464, min: 0 },
-  //     items: 1,
-  //     slidesToSlide: 1
-  //   }
-  // };
+  const [liked, setLiked] = useState(false);
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5,
+      slidesToSlide: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1
+    }
+  };
 
-  // cor do coração #e0245e
+  const renderLikedProduct = () => {
+    if (liked) {
+      return <HeartFilled onClick={() => setLiked(false)} style={{ color: '#e0245e', fontSize: 15 }} />
+    };
+
+    return <HeartOutlined onClick={() => setLiked(true)} style={{ color: '#c8c8c8', fontSize: 15 }} />
+  };
 
   return (
     <HomeSection>
@@ -50,9 +57,133 @@ function Home() {
         Produtos novos
       </CardSectionTitle>
 
-      <ProductCard>
+      <Carousel
+        responsive={responsive}
+        infinite={true}
+      >
+        <ProductCard>
+          <CardActions>
+            {/* <HeartOutlined style={{ fontSize: 15, color: '#c8c8c8' }} /> */}
+            {renderLikedProduct()}
+            <ShoppingCartOutlined style={{ fontSize: 20, color: '#c8c8c8' }} />
+          </CardActions>
+          <CardInfo>
+            <img src={itemImage} width={150} alt="Relógio" />
+            <CardTitle>
+              Relógio Rolex
+            </CardTitle>
+            <CardPrice>
+              R$120,00
+            </CardPrice>
+          </CardInfo>
+          <CardShow>
+            Ver mais
+          </CardShow>
+        </ProductCard>
+
+        <ProductCard>
+          <CardActions>
+            {/* <HeartOutlined style={{ fontSize: 15, color: '#c8c8c8' }} /> */}
+            {renderLikedProduct()}
+            <ShoppingCartOutlined style={{ fontSize: 20, color: '#c8c8c8' }} />
+          </CardActions>
+          <CardInfo>
+            <img src={itemImage} width={150} alt="Relógio" />
+            <CardTitle>
+              Relógio Rolex
+            </CardTitle>
+            <CardPrice>
+              R$120,00
+            </CardPrice>
+          </CardInfo>
+          <CardShow>
+            Ver mais
+          </CardShow>
+        </ProductCard>
+
+        <ProductCard>
+          <CardActions>
+            {/* <HeartOutlined style={{ fontSize: 15, color: '#c8c8c8' }} /> */}
+            {renderLikedProduct()}
+            <ShoppingCartOutlined style={{ fontSize: 20, color: '#c8c8c8' }} />
+          </CardActions>
+          <CardInfo>
+            <img src={itemImage} width={150} alt="Relógio" />
+            <CardTitle>
+              Relógio Rolex
+            </CardTitle>
+            <CardPrice>
+              R$120,00
+            </CardPrice>
+          </CardInfo>
+          <CardShow>
+            Ver mais
+          </CardShow>
+        </ProductCard>
+
+        <ProductCard>
+          <CardActions>
+            {/* <HeartOutlined style={{ fontSize: 15, color: '#c8c8c8' }} /> */}
+            {renderLikedProduct()}
+            <ShoppingCartOutlined style={{ fontSize: 20, color: '#c8c8c8' }} />
+          </CardActions>
+          <CardInfo>
+            <img src={itemImage} width={150} alt="Relógio" />
+            <CardTitle>
+              Relógio Rolex
+            </CardTitle>
+            <CardPrice>
+              R$120,00
+            </CardPrice>
+          </CardInfo>
+          <CardShow>
+            Ver mais
+          </CardShow>
+        </ProductCard>
+        <ProductCard>
+          <CardActions>
+            {/* <HeartOutlined style={{ fontSize: 15, color: '#c8c8c8' }} /> */}
+            {renderLikedProduct()}
+            <ShoppingCartOutlined style={{ fontSize: 20, color: '#c8c8c8' }} />
+          </CardActions>
+          <CardInfo>
+            <img src={itemImage} width={150} alt="Relógio" />
+            <CardTitle>
+              Relógio Rolex
+            </CardTitle>
+            <CardPrice>
+              R$120,00
+            </CardPrice>
+          </CardInfo>
+          <CardShow>
+            Ver mais
+          </CardShow>
+        </ProductCard>
+        <ProductCard>
+          <CardActions>
+            {/* <HeartOutlined style={{ fontSize: 15, color: '#c8c8c8' }} /> */}
+            {renderLikedProduct()}
+            <ShoppingCartOutlined style={{ fontSize: 20, color: '#c8c8c8' }} />
+          </CardActions>
+          <CardInfo>
+            <img src={itemImage} width={150} alt="Relógio" />
+            <CardTitle>
+              Relógio Rolex
+            </CardTitle>
+            <CardPrice>
+              R$120,00
+            </CardPrice>
+          </CardInfo>
+          <CardShow>
+            Ver mais
+          </CardShow>
+        </ProductCard>
+      </Carousel>
+
+      {/* <ProductCard>
         <CardActions>
           <HeartOutlined style={{ fontSize: 15, color: '#c8c8c8' }} />
+          {renderLikedProduct()}
           <ShoppingCartOutlined style={{ fontSize: 20, color: '#c8c8c8' }} />
         </CardActions>
         <CardInfo>
@@ -67,7 +198,7 @@ function Home() {
         <CardShow>
           Ver mais
         </CardShow>
-      </ProductCard>
+      </ProductCard> */}
     </HomeSection>
   );
 };
