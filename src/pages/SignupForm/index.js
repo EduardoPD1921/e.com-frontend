464adfa216afe9d065bbba32a837405a0c09a9e1
Eduardo Gomes Heleno <eduardo.gheleno@gmail.com>
2021-08-13 09:50:31 -0300

    :boom: pulling out the page to choose between types of signup

---
+++ src/pages/SignupForm/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SignupForm() {
+  return (
+    <div>
+      test
+    </div>
+  );
+};
+
+export default SignupForm;
--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
-import RegisterTypeSelector from '../pages/RegisterTypeSelector';
 
 function Routes() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/registerTypeSelector" exact component={RegisterTypeSelector} />
       </Switch>
     </Router>
   );
