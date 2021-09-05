5b0f136816e50a6e0b02b39155bf26dfe5603d0c
Eduardo Gomes Heleno <eduardo.gheleno@gmail.com>
2021-09-05 16:24:41 -0300

    :sparkles: Bookmark merthod errors handled

--- src/Context/LikeContext.js
+++ src/Context/LikeContext.js
@@ -7,28 +7,38 @@ function LikeProvider({ children }) {
   const [likedProducts, setLikedProducts] = useState([]);
 
   useEffect(() => {
-    api.get('/user/getLikedProducts')
-      .then(resp => setLikedProducts(resp.data.likedProducts))
-      .catch(error => console.log(error.response));
+    const userToken = localStorage.getItem('token');
+
+    if (userToken) {
+      api.get('/user/getLikedProducts')
+        .then(resp => setLikedProducts(resp.data.likedProducts))
+        .catch(error => console.log(error.response));
+    };
   }, []);
 
   function handleLike(productId) {
     const data = { productId };
+
     api.put('/user/likeProduct', data)
-      .then(resp => {
-        setLikedProducts(prevState => [...prevState, productId]);
-      })
-      .catch(error => console.log(error.response));
+      .then(resp => setLikedProducts(prevState => [...prevState, productId]))
+      .catch(error => errorHandler(error.response.data));
   };
 
   function handleUnlike(productId) {
     const data = { productId };
+
     api.put('/user/unlikeProduct', data)
-      .then(resp => {
-        setLikedProducts(prevState => prevState.filter(e => e !== productId));
-      })
-      .catch(error => console.log(error.response));
+      .then(resp => setLikedProducts(prevState => prevState.filter(e => e !== productId)))
+      .catch(error => errorHandler(error.response.data));
   }
+
+  function errorHandler(error) {
+    if (error === 'access-denied') {
+      window.location.replace('/signIn');
+    };
+
+    console.log(error);
+  };
 
   return (
     <LikeContext.Provider value={{ handleLike, handleUnlike, likedProducts }}>
